a6e4c0926a0b5670b11f3ad08a38e566629ba859
chetra-seng <sengchetra.love@icloud.com>
2023-10-25 11:01:56 +0700

    Added date

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,14 @@
+
+
 <main>
   <h1>Hello world</h1>
   <input type="file" />
+  <button on:click={() => {
+    const now = Date.now().toString();
+    localStorage.setItem("name", now)
+    alert(now);
+  }}>Save</button>
+  <button on:click={() => {
+    alert(localStorage.getItem("name"))
+  }}>Get</button>
 </main>
